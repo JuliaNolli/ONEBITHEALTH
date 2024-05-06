c35e8532c1bd59cd033ed4732a9369ea52fbb889
JuliaNoli <julia.noli1b3@gmail.com>
2024-05-06 12:01:24 -0300

    função calcular imc

--- SRC/components/Form/index.js
+++ SRC/components/Form/index.js
@@ -1,7 +1,18 @@
-import React from "react";
 import { View, TextInput, Button, Text } from "react-native";
+import React, { UseState, useState } from "react"; 
 
 export default function Form(){
+
+  const [altura, setAltura]  = useState(null)
+  const [peso, setPeso] = useState(null)
+  const [messageIMC, setMessageImc] = useState("digite seu peso e altura");
+  const [imc, setImc] = useState(null)
+  const [textButton, setTextButton] = useState("CALCULAR IMC")
+
+  function imcCalculator(){
+    return setImc((peso/(altura*altura)).toFixed(2))
+  }
+
     return(
       <View>
         <View>
@@ -17,7 +28,7 @@ export default function Form(){
             keyboardType="Numeric"
             />
 
-            <Button title="Calcular"/>
+            <Button title={textButton}/>
 
         </View>
       </View>
